1efbca6f33e96dd450877cf7628f0134a35fe3cb
Gage Thornberry <gagedoug13@gmail.com>
2020-12-03 21:30:28 -0700

    most common keywords function is working.

--- src/components/Main.js
+++ src/components/Main.js
@@ -8,7 +8,11 @@ export default class Main extends Component {
     componentDidMount() {
         this.countChannelViews("JRE Clips")
         this.listOfTopChannels()
+        this.listOfTopKeywords()
+        // console.log(data)
     }
+
+    
 
     listOfTopChannels = () => {
         let channels = {}
@@ -27,11 +31,11 @@ export default class Main extends Component {
             arrayOfChannels.push([obj, channels[obj]])
         }
         
-       let sorted = arrayOfChannels.sort(function(a, b) {
+       let sortedList = arrayOfChannels.sort(function(a, b) {
             return b[1] - a[1] 
         })
-        // console.log(sorted)
-        // return sorted
+        // console.log(sortedList)
+        // return sortedList
     }
     
     countChannelViews = (name) => {
@@ -43,7 +47,35 @@ export default class Main extends Component {
                 }
             }
         }
-         console.log(counter)
+        //  console.log(counter)
+    }
+
+    listOfTopKeywords = () => {
+        let channels = {}
+        for (let i=0; i<data.length; i++) {
+            if (data[i]["subtitles"]) {
+                const channel = data[i]["subtitles"][0]["name"].split(' ')
+                // console.log(channel)
+               for (let keyword of channel) {
+                   if (!channels[keyword]) {
+                       channels[keyword] = 1
+                   } else {
+                       channels[keyword]++
+                   }
+               }
+            }
+        }
+        // console.log(channels)
+        let arrayOfKeywords = []
+        for (let obj in channels) {
+            arrayOfKeywords.push([obj, channels[obj]])
+        }
+        
+       let sortedList = arrayOfKeywords.sort(function(a, b) {
+            return b[1] - a[1] 
+        })
+        console.log(sortedList)
+
     }
 
     render() {
