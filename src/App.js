4cdad5e66fdd68862fcff695c902044738bd7e9d
Gage Thornberry <gagedoug13@gmail.com>
2020-12-18 23:09:48 -0700

    styled new components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import Main from './components/Main'
+import Home from './components/Home'
 
 
 function App() {
   // console.log({data})
   return (
     <div className="App">
-     
+     <Home />
      <Main />
     </div>
   );
---
+++ src/components/Home.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+export default function Home() {
+    return (
+        <div className='homeDiv'>
+            <div className='welcomeDiv'>
+                <h1>Welcome to YouTube Data Visualizer</h1>
+            </div>
+
+            <div className='fileInstructionsDiv'>
+                <button className='fileButton'>choose file</button>
+                <h3 className='or'>or</h3>
+                <button className='instructionsButton'>see instructions</button>
+            </div>
+
+            <div className='instructionsDiv'>
+                <p>how it works</p>
+                <h3>You download your raw YouTube data from google.takeouts.com and we 
+                    visualize it for you. All you have to do is follow the instructions 
+                    above and you'll be able to see your most viewed channels, videos, 
+                    and key words from your YouTube history.</h3>
+            </div>
+          
+        </div>
+    )
+}
